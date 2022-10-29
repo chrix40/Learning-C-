6f779ad70179490ccce7891610221ae1ccfb5544
Christian Roberto <74983189+chrix40@users.noreply.github.com>
2022-10-29 12:05:13 -0400

    Update std.cpp

--- Basic/std.cpp
+++ Basic/std.cpp
@@ -5,5 +5,5 @@ int main() {
   std::string hello = "Hello World!";
   std::cout << hello << std::endl;
   return 0; //  It is common to see some programs running without the standard library
-            // in this case it can be changed by the keyword st, followed by the operator :: for string (and cout) objects:
+            // in this case it can be changed by the keyword std, followed by the operator :: for string (and cout) objects:
 }
