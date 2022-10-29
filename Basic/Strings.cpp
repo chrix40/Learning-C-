97f7a22841a3d23bea5db403dedb79ac75dcd0e5
Christian <christian2020022014@gmail.com>
2022-10-29 11:26:12 -0400

    Update

--- Basic/Strings.cpp
+++ Basic/Strings.cpp
@@ -7,10 +7,14 @@ int main(){
     cout << s << endl;
     string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     cout << "The txt is: " << txt << endl;
-    txt[0] = 'J';
-    cout << "The txt now is: " << txt; // this change the caracter in position [0]
+    txt[0] = 'J'; // this change the caracter in position [0]
+    cout << "The txt now is: " << txt << endl; 
     
-    string a = "We are the so-called \"Vikings\" from the north."; // this keeps the quotes in case you need to keep in some string
+    string a, b, c;
+    a = "We are the so-called \"Vikings\" from the north."; // this keeps the quotes in case you need to keep in some string
+    b = "It\'s alright.";
+    c = "The character \\ is called backslash.";
+    cout << a << endl << b << endl << c << endl;
     
     return 0;
 }
