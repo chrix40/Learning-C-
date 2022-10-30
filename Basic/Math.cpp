34e981f719e2edbd3ac34dba94f24d57038fd414
Christian <christian2020022014@gmail.com>
2022-10-30 04:04:42 -0400

    Update

--- Basic/Math.cpp
+++ Basic/Math.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
     
-	return 0;
-}
-
     // int a, b, sum;
     // cout << "Type a first number: " << endl;
     // cin >> a;
     // cout << "Type a second number: " << endl;
     // cin >> b;
     // sum = a+b;
-    // cout << "Sum between a and b is: " << sum;
+    // cout << "Sum between a and b is: " << sum << endl;
+
+    cout << max(5, 10) << '\n'; // the max(x,y) function can be used to find the highest value of x and y
+    cout << min(5, 10) << '\n'; // the min(x,y) function can be used to find the lowest value of x and y
+    cout << sqrt(64) << '\n'; // sqrt (square root)
+    cout << round(2.6) << '\n'; // round (rounds a number)
+    cout << log(2) << '\n'; // log (natural logarithm)
+	return 0;
+}
+
+
 
     /* cout << "Hello World! \n" ;
     cout << "I am learning C++ \n";
